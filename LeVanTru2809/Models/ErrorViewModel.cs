ef8513ce978f75259f17615492e302f46a3fcf55
LeVanTru1921050714 <lee27052001@gmail.com>
2022-10-02 23:13:51 +0700

    first commit

---
+++ LeVanTru2809/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeVanTru2809.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
